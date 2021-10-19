29d2e1a984be5cb9bd4faa4e99c51a2acb9b8c8b
Splines <dominic-plein@gmx.de>
2021-10-20 01:12:16 +0200

    [Py] Implement Chimera embedding model and test

--- python/tests/test_graphs.py
+++ python/tests/test_graphs.py
@@ -38,7 +38,6 @@ class Test_Graphs(TestCase):
         edges = G._get_edges()
         adj_matrix = G._get_adj_matrix()
 
-        # --- Assert
         edges_assert = [(0, 4, 0), (0, 5, 0), (0, 6, 0), (0, 7, 0),
                         (1, 4, 0), (1, 5, 0), (1, 6, 0), (1, 7, 0),
                         (2, 4, 0), (2, 5, 0), (2, 6, 0), (2, 7, 0),
@@ -57,6 +56,63 @@ class Test_Graphs(TestCase):
                              [0, 0, 0, 0, -1, -1, -1, -1]]
         self.assertEqual(edges, edges_assert)
         self.assertEqual(adj_matrix, adj_matrix_assert)
+
+    def test_chimera_embedding_edge_embeddings(self):
+        G = ChimeraEmbedding()
+
+        # --- Add edges
+        try:
+            G.add_edge_embedding(4, 0)
+            G.add_edge_embedding(4, 1)
+            G.add_edge_embedding(4, 2)
+            G.add_edge_embedding(4, 3)
+
+            G.add_edge_embedding(0, 4)
+            G.add_edge_embedding(0, 5)
+            G.add_edge_embedding(0, 6)
+            G.add_edge_embedding(0, 7)
+        except IndexError:
+            self.fail('Did not expect an IndexError when adding')
+
+        for i in range(4):
+            self.assertRaises(IndexError, G.add_edge_embedding, 0, i)
+            self.assertRaises(IndexError, G.add_edge_embedding, i, 0)
+            self.assertRaises(IndexError, G.add_edge_embedding, 4, i+4)
+            self.assertRaises(IndexError, G.add_edge_embedding, i+4, 4)
+
+        # --- Remove edge
+        try:
+            G.remove_edge_embedding(4, 0)
+            G.remove_edge_embedding(4, 1)
+            G.remove_edge_embedding(4, 2)
+            G.remove_edge_embedding(4, 3)
+
+            # edge (0,4) go already already removed one line above
+            G.remove_edge_embedding(0, 5)
+            G.remove_edge_embedding(0, 6)
+            G.remove_edge_embedding(7, 0)  # order should not matter
+        except IndexError:
+            self.fail('Did not expect an IndexError when removing')
+
+        # Already removed those edge embeddings, so should result in error
+        # due to symmetric adjacency matrix (undirected graph)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 4, 0)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 5, 0)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 6, 0)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 7, 0)
+
+        # Edges that cannot possibly have an embedding
+        for i in range(4):
+            self.assertRaises(IndexError, G.remove_edge_embedding, 0, i)
+            self.assertRaises(IndexError, G.remove_edge_embedding, i, 0)
+            self.assertRaises(IndexError, G.remove_edge_embedding, 4, i+4)
+            self.assertRaises(IndexError, G.remove_edge_embedding, i+4, 4)
+
+        # Edges that could possibly have an embedding, but have not (anymore)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 1, 4)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 1, 5)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 1, 6)
+        self.assertRaises(IndexError, G.remove_edge_embedding, 1, 7)
 
 
 if __name__ == '__main__':
