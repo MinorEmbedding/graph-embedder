ab41864f4c088ac1e4455a0f9af132cfe57d9b6e
Splines <dominic-plein@gmx.de>
2022-05-14 19:40:02 +0200

    Evaluate different population sizes
    Also init plot of how many valid embeddings were found including the
    average number of generations needed. (Per population size)
    Always referring to K8 with max_generations=600 and a 5x5 Chimera grid.

--- python/plots/how_many_generations/how_many_generations_all.py
+++ python/plots/how_many_generations/how_many_generations_all.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-names = ['2x2', '3x3', '4x4', '5x5', '6x6', '7x7', '8x8', '9x9']
+names = [str(i) for i in range(1, 15)]
 
 
 # --- Prepare data
 Data = namedtuple('Data', ['unique', 'counts'])
 datas = []
 for name in names:
-    with open(f'./data/how_many_generations_{name}_k8_different_grid_sizes.txt', 'r') as f:
+    with open(f'./data/population_size_100_runs/how_many_generations_5x5_100_600_max_gen_k8_population_size_{name}.txt', 'r') as f:
         data = f.read().splitlines()
         data = [int(value) for value in data]
         unique, counts = np.unique(data, return_counts=True)
@@ -40,7 +40,7 @@ plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
 
 
 # --- Plot
-rows = 2
+rows = 3
 cols = 4
 fig, axs = plt.subplots(rows, cols, figsize=(14, 9))
 
@@ -56,25 +56,29 @@ for r in range(rows):
         color = '#E53054'
         ax.bar(datas[i][0], datas[i].counts, color=color)
         # ax.axhline(0, color='grey', linewidth=0.8)
-        ax.set_title(names[i])
+        ax.set_title(f'population size {names[i]}')
 
-        major = 5 if max(datas[i].unique) <= 60 else 10
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(major))
+        # major = 5 if max(datas[i].unique) <= 60 else 10
+        # ax.xaxis.set_major_locator(ticker.MultipleLocator(major))
 
-        minor = 1 if max(datas[i].unique) <= 60 else 5
-        ax.xaxis.set_minor_locator(ticker.MultipleLocator(minor))
+        # minor = 1 if max(datas[i].unique) <= 60 else 5
+        # ax.xaxis.set_minor_locator(ticker.MultipleLocator(minor))
 
-        y_max = 60 if r == 0 else 70
-        ax.set_ylim(top=y_max)
+        # y_max = 60 if r == 0 else 70
+        # ax.set_ylim(top=75)
+        # if max(datas[i].counts) >= 70:
+        #     ax.set_ylim(top=90)
+        # else:
+        #     ax.set_ylim(top=75)
         # ax.set_xlim(0, 100)
 
         i += 1
 
 # Label
-axs[1, 0].set_ylabel('Occurrences')
-axs[1, 0].set_xlabel('Generations needed')
+# axs[3, 0].set_ylabel('Occurrences')
+# axs[3, 0].set_xlabel('Generations needed')
 
 # ax.set_title('Generations needed for the crossed house puzzle')
-plt.subplots_adjust(left=0.05, top=0.96, right=0.98, bottom=0.06, hspace=0.2, wspace=0.2)
+plt.subplots_adjust(left=0.05, top=0.96, right=0.98, bottom=0.06, hspace=0.4, wspace=0.25)
 # plt.tight_layout(pad=0.2, w_pad=0.1, h_pad=0.1, rect=(0.05, 0.05, 0.95, 0.95))
 plt.show()
