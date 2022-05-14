d5ab0c9b14f19d33d3eb6fe90d2343566b177d27
Splines <dominic-plein@gmx.de>
2022-05-14 19:50:53 +0200

    Evaluate K8 with different probabilites
    (for the "extend to free neighbor" mutation)

--- python/plots/how_many_generations/how_many_embedded.py
+++ python/plots/how_many_generations/how_many_embedded.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-names = [str(i) for i in range(1, 21)]
-# names = ['0.0', '0.11']
+# names = [str(i) for i in range(1, 21)]
+names = ['0.0', '0.11', '0.22', '0.33', '0.44', '0.56', '0.67', '0.78', '0.89', '1.0']
 embedded = []
 average_generations = []
 
 # --- Prepare data
 for name in names:
-    filename = f'./data/population_size_100_runs/how_many_generations_5x5_100_600_max_gen_k8_population_size_{name}.txt'
-    # filename = f'./data/extend_to_free_prob_50_runs/how_many_generations_5x5_50_600_max_gen_k8_extend_to_free_prob{name}.txt'
+    # filename = f'./data/population_size_100_runs/how_many_generations_5x5_100_600_max_gen_k8_population_size_{name}.txt'
+    filename = f'./data/extend_to_free_prob_50_runs/how_many_generations_5x5_50_600_max_gen_k8_extend_to_free_prob_{name}.txt'
     with open(filename, 'r') as f:
         data = f.read().splitlines()
         data = [int(value) for value in data]
@@ -45,7 +45,7 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 color = '#E53054'
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.set_ylabel('Occurrences of valid embeddings')
-ax.set_xlabel('Population size')
+ax.set_xlabel('Probability of using the "extend to free neighbor" mutation')
 ax.set_ylim(0, 100)
 # ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
 # ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
