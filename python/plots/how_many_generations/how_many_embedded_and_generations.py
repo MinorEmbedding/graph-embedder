786d6ebb848b879d523170c7a45945140ce98727
Splines <dominic-plein@gmx.de>
2022-05-16 22:32:28 +0200

    Add results from server (probabilities and population size)

--- python/plots/how_many_generations/how_many_embedded_and_generations.py
+++ python/plots/how_many_generations/how_many_embedded_and_generations.py
@@ -3,7 +3,7 @@ import matplotlib.ticker as ticker
 import numpy as np
 
 names = [str(i) for i in range(2, 15)]
-# names = np.linspace(0.0, 0.2, num=10)
+# names = np.linspace(0.0, 1.0, num=20)
 # names = [str(round(v, 2)) for v in names]
 # names.append('0.22')
 # names.append('0.24')
@@ -17,13 +17,15 @@ names = [str(i) for i in range(2, 15)]
 
 embedded = []
 average_generations = []
-max_total = 200
+max_total = 300
 
 # --- Prepare data
 for name in names:
     # filename = f'./data_after_bug_fix/k8_popsize/howManyGenerations_5x5_10_600gen_k8popsize{name}.txt'
     # filename = f'./data_after_bug_fix/k8_probabilities_free/howManyGenerations_5x5_20_600gen_k80.0-0.2prob{name}.txt'
-    filename = f'./data_after_bug_fix/k_graphs/howManyGenerations_2x2_200_600gen_6popsize_k{name}.txt'
+    # filename = f'./data_after_bug_fix/k_graphs/howManyGenerations_16x16_100_600gen_6popsize_k{name}.txt'
+    # filename = f'./data_after_bug_fix/k8_probabilities_free/server-run/howManyGenerations_5x5_300_600gen_k8prob{name}.txt'
+    filename = f'./data_after_bug_fix/k8_popsize/server-run/howManyGenerations_5x5_300_600gen_k8popsize{name}.txt'
     with open(filename, 'r') as f:
         data = f.read().splitlines()
         data = [int(value) for value in data]
@@ -56,23 +58,23 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 # --- Plot
 fig, ax = plt.subplots(figsize=(11, 6))
-# ax.set_xlabel('Population size')
+ax.set_xlabel('Population size')
 # ax.set_xlabel('Probability for "extend to free neighbors" heuristic')
-ax.set_xlabel('K graph')
+# ax.set_xlabel('K graph')
 
 # Average generations
 color_blue = '#2981B3'
 # color_blue = '#2EB4FF'
 ax.set_ylabel('Average number of generations needed', color=color_blue)
 # ax.plot(names, average_generations, color=color_blue, marker='o')
-ax.bar(names, average_generations, color=color_blue)
+ax.bar(names, average_generations, color=color_blue, fill=False, edgecolor=color_blue)
 ax.tick_params(axis='y', labelcolor=color_blue)
 
 
 ax2 = ax.twinx()
 # Number embedded
 color_red = '#E53054'
-color_red_bright = '#FF4A6D'
+# color_red = '#FF4A6D'
 ax2.set_ylabel('Occurrences of valid embeddings', color=color_red)
 ax2.set_ylim(0, max_total+5)
 # ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
--- python/plots/how_many_generations/how_many_generations_all.py
+++ python/plots/how_many_generations/how_many_generations_all.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-names = [str(i) for i in range(2, 10)]
+names = [str(i) for i in range(6, 14)]
 
 
 # --- Prepare data
@@ -12,7 +12,7 @@ Data = namedtuple('Data', ['unique', 'counts'])
 datas = []
 for name in names:
     # filename = f'./data/population_size_100_runs/how_many_generations_5x5_100_600_max_gen_k8_population_size_{name}.txt'
-    filename = f'./data_after_bug_fix/k_graphs/howManyGenerations_2x2_200_600gen_6popsize_k{name}.txt'
+    filename = f'./data_after_bug_fix/k_graphs/howManyGenerations_16x16_100_600gen_6popsize_k{name}.txt'
     with open(filename, 'r') as f:
         data = f.read().splitlines()
         data = [int(value) for value in data]
@@ -59,7 +59,7 @@ for r in range(rows):
         ax.bar(datas[i][0], datas[i].counts, color=color)
         # ax.axhline(0, color='grey', linewidth=0.8)
         # ax.set_title(f'population size {names[i]}')
-        ax.set_title(f'$K_{names[i]}$')
+        ax.set_title(f'$K_{{{names[i]}}}$')
 
         # Same scale for most subfigures
         # maximum = max(datas[i].unique)
@@ -68,6 +68,8 @@ for r in range(rows):
         #     ax.set_xlim(-5, 100)
 
         ax.set_xlim(left=-5)
+        if i >= 2:
+            ax.set_xlim(left=-30)
 
         # major = 5 if max(datas[i].unique) <= 60 else 10
         # ax.xaxis.set_major_locator(ticker.MultipleLocator(major))
@@ -91,12 +93,12 @@ for r in range(rows):
         i += 1
 
 # Label
-axs[0, 0].set_xlim(-5, 20)
+# axs[0, 0].set_xlim(-5, 20)
 
 axs[1, 0].set_ylabel('Occurrences')
 axs[1, 0].set_xlabel('Generations needed')
 
 # ax.set_title('Generations needed for the crossed house puzzle')
-plt.subplots_adjust(left=0.05, top=0.96, right=0.98, bottom=0.06, hspace=0.4, wspace=0.25)
+plt.subplots_adjust(left=0.05, top=0.96, right=0.98, bottom=0.06, hspace=0.3, wspace=0.25)
 # plt.tight_layout(pad=0.2, w_pad=0.1, h_pad=0.1, rect=(0.05, 0.05, 0.95, 0.95))
 plt.show()
